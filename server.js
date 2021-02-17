f3f6b25265282602e74815685cfe81b1891b6ae6
Joshua Ben-Baba <josh45022@gmail.come>
2021-02-16 18:19:13 -0700

    Installed packages

--- server.js
+++ server.js
@@ -4,13 +4,15 @@ require('dotenv').config()
 const morgan = require('morgan')
 const mongoose = require('mongoose')
 const expressJwt = require("express-jwt")
+const path = require("path")
+const port = process.env.PORT || 7000;
 
 
 app.use(express.json())
 app.use(morgan('dev'))
 
 mongoose.connect(
-    'mongodb://localhost:27017/erasertimedb',
+    process.env.MONGODB_URI || 'mongodb://localhost:27017/erasertimedb',
     {
         useNewUrlParser: true,
         useUnifiedTopology: true,
@@ -38,6 +40,13 @@ app.use(
     }
 )
 
-app.listen(7000, ()=> {
+app.use(express.static(path.join(__dirname, "client", "build")))
+
+app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, "client", "build", "index.html"));
+})
+
+
+app.listen(port, ()=> {
     console.log('This server is running on Port 7000')
 })
